afbfbdb0d6967b9c9bccc3100ddfc031d451b18d
José Boné <79787418+jose-bone@users.noreply.github.com>
2021-07-09 04:59:29 +0200

    New route to Admin room

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,8 @@ function App() {
           <Route path="/" exact component={Home} />
           <Route path="/rooms/new" exact component={NewRoom} />
           <Route path="/rooms/:id" component={Room} />
+
+          <Route path="/admin/rooms/:id" component={AdminRoom} />
         </Switch>
       </AuthContextProvider>
     </BrowserRouter>
