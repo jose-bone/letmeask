cbbb32a74b00365b6e0b0a1430a931c390e4d24f
José Boné <79787418+jose-bone@users.noreply.github.com>
2021-07-24 21:25:49 +0200

    New buttons

--- src/pages/AdminRoom.tsx
+++ src/pages/AdminRoom.tsx
@@ -86,6 +86,19 @@ export function AdminRoom() {
                       type="button"
                       onClick={() => handleCheckQuestionAsAnswered(question.id)}
               >
+                      <img
+                        src={checkImg}
+                        alt="Marcar pergunta como respondida"
+                      />
+                    </button>
+                    <button
+                      type="button"
+                      onClick={() => handleHighlightQuestion(question.id)}
+                    >
+                      <img src={answerImg} alt="Dar destaque a pergunta" />
+                    </button>
+                  </>
+                )}
                 <button
                   type="button"
                   onClick={() => handleDeleteQuestion(question.id)}
